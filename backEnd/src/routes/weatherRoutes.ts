4ed007cb417a1b03bc577ad1457290768fa51160
anemoreira <adjanead1@gmail.com>
2024-08-27 20:20:07 -0300

    Delete backEnd directory

--- backEnd/src/routes/weatherRoutes.ts
+++
@@ -1,9 +0,0 @@
-// backend/src/routes/weatherRoutes.ts
-import { Router } from 'express';
-import { getWeather } from '../controllers/weatherController';
-
-const router = Router();
-
-router.post('/weather', getWeather);
-
-export default router;
