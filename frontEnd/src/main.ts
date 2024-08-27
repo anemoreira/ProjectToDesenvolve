519a54810e7fbe00bd37c1b0dcc1d370e8394599
anemoreira <adjanead1@gmail.com>
2024-08-27 20:20:38 -0300

    Delete frontEnd directory

--- frontEnd/src/main.ts
+++
@@ -1,9 +0,0 @@
-// frontend/src/main.ts
-import { createApp } from 'vue';
-import App from './App.vue';
-import Home from './views/Home.vue';
-
-const app = createApp(App);
-
-app.component('Home', Home);
-app.mount('#app');
